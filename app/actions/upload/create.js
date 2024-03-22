5304856c62970e95fd46a645bfb9d968b1073621
Vidar Eldøy <vidar@eldoy.com>
2024-03-22 03:06:53 +0100

    Keep file

--- app/actions/upload/create.js
+++ app/actions/upload/create.js
@@ -1,10 +1,5 @@
 module.exports = async function ($) {
-  return [
-    {
-      name: 'filename.svg',
-      size: '67',
-      type: 'image/svg',
-      url: 'https://firmalisten.no/img/firmalisten-logo-long-dark.svg'
-    }
-  ]
+  var file = $.files[0]
+  file.url = 'https://firmalisten.no/img/firmalisten-logo-long-dark.svg'
+  return [file]
 }
